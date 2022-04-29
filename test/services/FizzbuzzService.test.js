b340c7a9a6a8d46af9584a7d952f89bda74024a9
Jesús Álvarez <khisus19@gmail.com>
2022-04-28 21:26:03 -0500

    Agregadas todas las pruebas de fizzbuzz

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,9 +1,27 @@
 const FizzbuzzService = require("./../../lib/services/FizzbuzzService")
 
 describe("Test para FizzbuzzService", () => {
-    test("Validacion Fizzbuzz", () => {
-        const explorer = {name: "Explorer1", score: 1};
+    test("1. Validacion Fizzbuzz", () => {
+        const explorer = {name: "Explorer1", score: 15};
         const explorersValidated = FizzbuzzService.applyValidationInExplorer(explorer);
-        expect(explorersValidated.trick).toBe(1);
+        expect(explorersValidated.trick).toBe("FIZZBUZZ");
+    })
+
+    test("2. Validacion Fizz", () => {
+        const explorer = {name: "Explorer1", score: 3};
+        const explorersValidated = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(explorersValidated.trick).toBe("FIZZ");
+    })
+
+    test("3. Validacion Buzz", () => {
+        const explorer = {name: "Explorer1", score: 5};
+        const explorersValidated = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(explorersValidated.trick).toBe("BUZZ");
+    })
+
+    test("4. Validacion other score", () => {
+        const explorer = {name: "Explorer1", score: 2};
+        const explorersValidated = FizzbuzzService.applyValidationInExplorer(explorer);
+        expect(explorersValidated.trick).toBe(2);
     })
 })
