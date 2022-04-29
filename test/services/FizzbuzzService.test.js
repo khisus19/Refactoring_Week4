82b9cbc8c6a69b117b13203f2c6a4fbb7866628f
Jesús Álvarez <khisus19@gmail.com>
2022-04-29 13:54:05 -0500

    Pasando el lint-fix

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,27 +1,27 @@
-const FizzbuzzService = require("./../../lib/services/FizzbuzzService")
+const FizzbuzzService = require("./../../lib/services/FizzbuzzService");
 
 describe("Test para FizzbuzzService", () => {
     test("1. Validacion Fizzbuzz", () => {
         const explorer = {name: "Explorer1", score: 15};
         const explorersValidated = FizzbuzzService.applyValidationInExplorer(explorer);
         expect(explorersValidated.trick).toBe("FIZZBUZZ");
-    })
+    });
 
     test("2. Validacion Fizz", () => {
         const explorer = {name: "Explorer1", score: 3};
         const explorersValidated = FizzbuzzService.applyValidationInExplorer(explorer);
         expect(explorersValidated.trick).toBe("FIZZ");
-    })
+    });
 
     test("3. Validacion Buzz", () => {
         const explorer = {name: "Explorer1", score: 10};
         const explorersValidated = FizzbuzzService.applyValidationInExplorer(explorer);
         expect(explorersValidated.trick).toBe("BUZZ");
-    })
+    });
 
     test("4. Validacion other score", () => {
         const explorer = {name: "Explorer1", score: 2};
         const explorersValidated = FizzbuzzService.applyValidationInExplorer(explorer);
         expect(explorersValidated.trick).toBe(2);
-    })
-})
+    });
+});
